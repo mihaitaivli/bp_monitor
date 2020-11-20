4a103d994019f95e393b1321c405eaa4b92e23d7
Mihaita Ivli <mihaita.ivli@skybettingandgaming.com>
2020-11-20 20:47:01 +0000

    simple User query resolver

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -9,6 +9,7 @@ import (
 	"log"
 
 	"github.com/mihaitaivli/bp_monitor/dbUtils"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/mihaitaivli/bp_monitor/graph/generated"
@@ -17,8 +18,12 @@ import (
 
 var client = dbUtils.InitConnection()
 
+type MongoFields struct {
+	ID   primitive.ObjectID `bson:"_id, omitempty"`
+	Name string             `bson:"name"`
+}
+
 func (r *mutationResolver) AddUser(ctx context.Context, input model.NewUser) (*string, error) {
-	defer client.Disconnect(context.Background())
 	collection := client.Database("bp_log").Collection("users")
 
 	insertUserResult, err := collection.InsertOne(context.Background(), input)
@@ -28,16 +33,48 @@ func (r *mutationResolver) AddUser(ctx context.Context, input model.NewUser) (*s
 	}
 
 	insertedID := insertUserResult.InsertedID.(primitive.ObjectID).Hex()
-	log.Println("Successfully addes user", insertedID)
+
 	return &insertedID, nil
 }
 
 func (r *mutationResolver) AddRecord(ctx context.Context, input model.NewRecord) (*string, error) {
-	panic(fmt.Errorf("not implemented"))
+	// defer client.Disconnect(context.Background())
+	collection := client.Database("bp_log").Collection("records")
+
+	insertRecordResult, err := collection.InsertOne(context.Background(), input)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	insertedID := insertRecordResult.InsertedID.(primitive.ObjectID).Hex()
+
+	return &insertedID, nil
 }
 
 func (r *queryResolver) User(ctx context.Context, id string) (*model.User, error) {
-	panic(fmt.Errorf("not implemented"))
+	collection := client.Database("bp_log").Collection("users")
+
+	objID, error := primitive.ObjectIDFromHex(id)
+	if error != nil {
+		log.Println(error)
+		return nil, error
+	}
+
+	filter := bson.M{"_id": objID}
+	var result MongoFields
+
+	err := collection.FindOne(context.Background(), filter).Decode(&result)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	user := model.User{
+		ID:   result.ID.Hex(),
+		Name: result.Name,
+	}
+	return &user, nil
 }
 
 func (r *queryResolver) Record(ctx context.Context, id string) (*model.Record, error) {
